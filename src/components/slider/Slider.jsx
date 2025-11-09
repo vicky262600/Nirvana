95b50326dc2eae425ff519a7c3c7041f3f8c9257
Vikas Joshi <145147066+vicky262600@users.noreply.github.com>
2025-11-09 10:31:14 -0800

    slider updated

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -4,18 +4,17 @@ const Slider = () => {
   const slides = [
     {
       id: 1,
-      image:
-        'https://images.unsplash.com/photo-1596755094514-f87e34085b2c?ixlib=rb-4.0.3&auto=format&fit=crop&w=2400&q=80',
+      image: '/slide1.jpeg',
     },
     {
       id: 2,
       image:
-        'https://images.unsplash.com/photo-1551028719-00167b16eac5?ixlib=rb-4.0.3&auto=format&fit=crop&w=2400&q=80',
+        '/slide2.jpeg',
     },
     {
       id: 3,
       image:
-        'https://images.unsplash.com/photo-1595777457583-95e059d581b8?ixlib=rb-4.0.3&auto=format&fit=crop&w=2400&q=80',
+        '/slide3.jpeg',
     },
   ];
 
